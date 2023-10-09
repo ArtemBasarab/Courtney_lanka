a2385fab64ebf3d3171fe188d8ec5014f87810e5
Artem Basarab <artembasarab.work@gmail.com>
2023-10-09 12:59:00 +0300

    start

--- gulp/createImg.js
+++ gulp/createImg.js
@@ -3,13 +3,13 @@ import imagemin from 'gulp-imagemin'
 import browserSync from 'browser-sync'
 
 export const imgMin = () => {
-  return gulp.src('dist/img/*.*')
+  return gulp.src('dist/img/**/*.*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'))
 }
 
 export const copyImg = () => {
-  return gulp.src('src/img/*.*')
+  return gulp.src('src/img/**/*.*')
     .pipe(gulp.dest('dist/img'))
     .pipe(browserSync.stream())
 }
--- src/js/partials/navbar.js
+++ src/js/partials/navbar.js
@@ -1,18 +1,12 @@
-export const navbar = window.addEventListener("DOMContentLoaded", () => {
+export const navbar = window.addEventListener('DOMContentLoaded', () => {
   document
-    .querySelector(".navbar__burger-btn")
-    .addEventListener("click", () => {
+    .querySelector('.navbar__burger-btn')
+    .addEventListener('click', () => {
       document
-        .querySelector(".navbar__burger-btn")
-        .classList.toggle("burger-active");
+        .querySelector('.navbar__burger-btn')
+        .classList.toggle('burger-active')
       document
-        .querySelector(".navbar__nav-list")
-        .classList.toggle("nav-active");
-    });
-
-  document.querySelectorAll(".navbar__nav-link-dropdown").forEach((element) => {
-    element.addEventListener("click", () => {
-      element.classList.toggle("navbar__nav-link-dropdown-active");
-    });
-  });
-});
+        .querySelector('.navbar__nav-list')
+        .classList.toggle('nav-active')
+    })
+})
